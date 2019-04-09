acba2326964ec1e8809d8386d4868eec3c800d10
Zainan Victor Zhou <zzn@google.com>
2019-04-09 11:35:09 -0700

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    // 'plugin:prettier/recommended',
+    // 'prettier',
+    // 'prettier/vue'
+  ],
+  plugins: [
+    // 'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    semi: 'off',
+    indent: 'off',
+    'no-console': 'off'
+  }
+}
