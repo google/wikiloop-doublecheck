66a5c2ed3a9a7c9f49a515a0eff4d7a43145cb9b
Zainan Victor Zhou <zzn@google.com>
2019-04-11 18:37:21 -0700

    Proper way to import css

--- components/DiffBox.vue
+++ components/DiffBox.vue
@@ -4,9 +4,6 @@
   </div>
 </template>
 
-<style>
-  @import "https://www.mediawiki.org/w/load.php?modules=mediawiki.legacy.shared|mediawiki.diff.styles&only=styles";
-</style>
 <script>
 export default {
   props: [ "diffContent" ]
