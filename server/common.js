ee49b2c61a113e03a92f7b15021abb155c7f6ca9
Zainan Victor Zhou <zzn@google.com>
2019-08-26 16:08:56 -0700

    Add copyright header

--- server/common.js
+++ server/common.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const rp = require('request-promise');
 
 const logger = new (require('heroku-logger').Logger)({
@@ -252,7 +266,7 @@ async function getNewJudgementCounts(db, matcher = {}, offset = 0, limit = 10) {
         "_id":{
           "wikiRevId":"enwiki:905704873"
         },
-  
+
         "lastTimestamp":"1562791937",
         "recentChange":{
           "_id":"enwiki-1169325920",
