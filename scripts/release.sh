d649b204d6bd167a54fbf55f6e15ed4abe30619e
Zainan Victor Zhou <zzn@google.com>
2019-09-01 16:36:29 -0700

    Try again for building the binary

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,8 @@
 echo "release script called .. "
 
 # According to https://devcenter.heroku.com/articles/release-phase
+echo "rm -rf .nuxt"
+rm -rf .nuxt
 echo "npm run build # So the promoted heroku app in pipeline can have new ENV variables in the built binary. See "
 echo "AXIOS_BASE_URL = $AXIOS_BASE_URL"
 npm run build
