a1f93ecdebd40a8ea05169980222627058112cab
Zainan Victor Zhou <zzn@zzn.im>
2019-12-03 22:47:10 -0800

    Implement a basic sorting algorithm for queue/feed. part of #108,#111,#112.

--- pages/queue.vue
+++ pages/queue.vue
@@ -45,26 +45,55 @@
             },
             showPrev: async function() {
                 await sleep(300);
-                this.nextWikiRevIds.unshift(this.currentWikiRevId);
-                this.currentWikiRevId = this.prevWikiRevIds.shift();
+                this.nextWikiRevIds.push(this.currentWikiRevId);
+                this.currentWikiRevId = this.prevWikiRevIds.pop();
             },
             loadNextWikiRevId: async function() {
                 if (this.nextWikiRevIds.length === 0) await this.loadWikiRevIds();
 
                 // push into the current WikiRevId into the prev queue, and load a new one.
-                if (this.currentWikiRevId) this.prevWikiRevIds.push(this.currentWikiRevId);
-                this.currentWikiRevId = this.nextWikiRevIds.pop();
+                if (this.currentWikiRevId) this.prevWikiRevIds.unshift(this.currentWikiRevId);
+                this.currentWikiRevId = this.nextWikiRevIds.shift();
             },
 
+            _computeAndStorePriority: function (wikiRevId) {
+              // TODO(zzn): implement this function according to
+              // the design discussion of https://github.com/google/wikiloop-battlefield/issues/112
+
+              // Approach: using RevId as the quantifying measure.
+              let priority = parseInt(wikiRevId.split(':')[1]);
+              this.wikiRevIdToMeta[wikiRevId].meta = {};
+
+              // For 20% of time, we will ask the reviewer to review a randomly generated revision
+              // first somewhere, therefore, regardless.
+              if (Math.random() <= 0.2) {
+                priority += (REVIEW_QUEUE_SIZE); // and we don't consider any other factors.
+                this.wikiRevIdToMeta[wikiRevId].meta.random = true;
+              } else {
+                if (this.wikiRevIdToMeta[wikiRevId]) {
+                  let item = this.wikiRevIdToMeta[wikiRevId];
+                  // The more suspicious, the more priority. We have not yet honored "hardcase" so far.
+                  if (item.ores && item.ores.damaging && item.ores.damaging.true >= 0.5) {
+                    priority += (REVIEW_QUEUE_SIZE);
+                  }
+                  if (item.ores && item.ores.damaging && item.ores.goodfaith.false >= 0.5) {
+                    priority += (REVIEW_QUEUE_SIZE);
+                  }
+                }
+              }
+              this.wikiRevIdToMeta[wikiRevId].meta.priority = priority;
+            },
             _sortWikiRevIds: async function(wikiRevIds) {
               // TODO(zzn): implement this function according to
               // the design discussion of https://github.com/google/wikiloop-battlefield/issues/112
-              return wikiRevIds; // currently by passing
+              return wikiRevIds.sort((wikiRevId1, wikiRevId2) =>
+                  this.wikiRevIdToMeta[wikiRevId2].meta.priority - this.wikiRevIdToMeta[wikiRevId1].meta.priority);
             },
 
            /** An internal function to load more revision Ids with time direction
-           *
-           * @param isNew if new, we are querying newer revisions of our timewindow, else
+            *
+            * @param limit a number of limit of revisions to be returned.
+            * @param isNew if new, we are querying newer revisions of our timewindow, else
             *   we are querying more older revisions.
             *   ----------------------------------------
             *  older <-||   revisions cached client  ||-> newer
@@ -92,11 +121,15 @@
                 }
               }
 
-              // TODO(zzn): create a new endpoint of `/api/revisions/list`
-              let apiPage = await this.$axios.$get(`/api/revisions/list?${urlParams.toString()}`);
+              // TODO(zzn): create a new endpoint of `/api/recentchanges/list`
+              let apiPage = await this.$axios.$get(`/api/recentchanges/list?${urlParams.toString()}`);
               let _timestamps = apiPage.map(item => item.timestamp);
               this.maxTimestamp = Math.max(this.maxTimestamp,..._timestamps);
               this.minTimestamp = Math.min(this.minTimestamp,..._timestamps);
+
+              // TODO(zzn): consider a better way to cache it.
+              apiPage.forEach(item => this.wikiRevIdToMeta[item.wikiRevId] = item);
+              apiPage.forEach(item => this._computeAndStorePriority(item.wikiRevId));
               return apiPage.map(item => item.wikiRevId);
             },
 
@@ -111,7 +144,7 @@
                   REVIEW_QUEUE_SIZE - this.nextWikiRevIds.length, false);
               this.nextWikiRevIds = this.nextWikiRevIds.concat(olderWikiRevIds);
             }
-            this.nextWikiRevIds = this._sortWikiRevIds(this.nextWikiRevIds);
+            this.nextWikiRevIds = await this._sortWikiRevIds(this.nextWikiRevIds);
           }
         },
 
