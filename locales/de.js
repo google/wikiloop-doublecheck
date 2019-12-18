3949a98313576906c16dc6338047dbb4d56f3796
Ali GOREN <goren.ali@yandex.com>
2019-12-19 01:49:22 +0300

    Refactor locale strings (#130)
    * changed nuxt configuration from hardcoded language strings to seperated language strings
    
    * Languages refactored. I changed hard-coded options. Languages ​​will come from an external file now. This should fixes #129

---
+++ locales/de.js
@@ -0,0 +1,15 @@
+module.exports = {
+    LoginMenuItem: "Anmelden",
+    LogoutMenuItem: "Abmelden",
+    ContributionsMenuItem: "Beiträge",
+    Anonymous: "Anonym",
+    Me: "Ich",
+    Someone: "Jemand",
+    EditSummaryLabel: "Zusammenfassung bearbeiten",
+    LooksGoodBtnLabel: "Sieht gut aus",
+    NotSureBtnLabel: "Nicht sicher",
+    ShouldRevertBtnLabel: "Sollte zurücksetzen",
+    NextBtnLabel: "Nächste",
+    Loading: "Beladung",
+    EditedTimeLabel: "Bearbeitet"
+}
