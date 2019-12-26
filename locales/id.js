b1f7e814b2178c3dabda2a0bd6cb98daacd4d8f5
Hamdanil Rasyid <hamdanil.rasyid@gmail.com>
2019-12-26 11:26:26 -0800

    Create id.js for Indonesian

---
+++ locales/id.js
@@ -0,0 +1,16 @@
+module.exports = {
+    LoginMenuItem: "Masuk log",
+    LogoutMenuItem: "Keluar log",
+    ContributionsMenuItem: "Kontribusi",
+    Anonymous: "Anonim",
+    Me: "Aku",
+    Someone: "Seseorang",
+    EditSummaryLabel: "Ringkasan suntingan",
+    LooksGoodBtnLabel: "Kelihatan bagus",
+    NotSureBtnLabel: "Tidak yakin",
+    ShouldRevertBtnLabel: "Patut dibatalkan",
+    NextBtnLabel: "Selanjutnya",
+    Loading: "Sedang memuat",
+    EditedTimeLabel: "disunting",
+    DiffNotAvailable: "Perbedaan revisi tidak tersedia untuk saat ini. Silakan diulang membukanya. Kadang revisi yang terkena vandalisme sudah dihapus. Silakan periksa riwayat revisi artikel."
+}
