c0f3742f201584bf8e9ed117efb1702b6caa6092
Zainan Victor Zhou <zzn@google.com>
2020-04-08 10:07:07 -0700

    Add feature to allow POST or DELETE  a feed.

--- server/routes/feed.ts
+++ server/routes/feed.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, {Request, Response} from "express";
 import {FeedEnum, WatchCollectionFeed} from "../../server/feed/watch-collection-feed";
 
 export const feedRouter = express.Router();
@@ -6,3 +6,39 @@ export const feedRouter = express.Router();
 feedRouter.get("/:feed", async (req: Request, res: Response) => {
   res.send(await WatchCollectionFeed.sampleRevisions(FeedEnum[req.params.feed], parseInt(req.query.size) || 100));
 });
+
+if (process.env.FEED_WIKITRUST_TOKEN) {
+  // FEED_WIKITRUST_TOKEN=<token> curl -H "Content-Type: application/json" -H "WikiLoopToken:$FEED_WIKITRUST_TOKEN" -X POST -d @./test/testdata/wikitrust_feed.json http://localhost:3000/api/feed/WIKITRUST
+  feedRouter.post("/:feed", async (req: Request, res: Response) => {
+    // Validation
+    // TODO(xinbenlv): consider use `express-validator`
+    // TODO(xinbenlv): change to MongoDB
+    if (req.params.feed == 'WIKITRUST' && req.header('WikiLoopToken') == process.env.FEED_WIKITRUST_TOKEN) {
+      const mongoose = require('mongoose');
+      await mongoose.connection.db.collection(`WatchCollection_WIKITRUST`)
+        .insertMany(req.body.content);
+        // TODO(xinbenlv) add logic to allow validating content format, or consider use gRPC
+      res.send(`ok`);
+    } else {
+      res.status(403).send(
+        `The provided feed ${req.body.feed} or feedToken ${req.body.feedToken} combination is invalid.`);
+    }
+  });
+
+  // FEED_WIKITRUST_TOKEN=<token> curl -H "Content-Type: application/json" -H "WikiLoopToken:$FEED_WIKITRUST_TOKEN" -X DELETE http://localhost:3000/api/feed/WIKITRUST
+  feedRouter.delete("/:feed", async (req: Request, res: Response) => {
+    // Validation
+    // TODO(xinbenlv): consider use `express-validator`
+    // TODO(xinbenlv): change to MongoDB
+    if (req.params.feed == 'WIKITRUST' && req.header('WikiLoopToken') == process.env.FEED_WIKITRUST_TOKEN) {
+      const mongoose = require('mongoose');
+      await mongoose.connection.db.collection(`WatchCollection_WIKITRUST`)
+        .drop();
+      // TODO(xinbenlv) add logic to allow validating content format, or consider use gRPC
+      res.send(`ok`);
+    } else {
+      res.status(403).send(
+        `The provided feed ${req.body.feed} or feedToken ${req.body.feedToken} combination is invalid.`);
+    }
+  });
+}
