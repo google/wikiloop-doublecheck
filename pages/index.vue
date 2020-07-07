3c8af18dfeacfe33d03a1048c293e6886644cf24
Sohom Datta <sohom.datta@learner.manipal.edu>
2020-07-07 21:44:50 +0530

    fix: added v-bind where raw variable names where getting leaked (#283)

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
                 ></RevisionCard>
         </template>
 
-      <b-modal id="modal-promote-login" title="$t('Label-Prompt-Login')">
+      <b-modal id="modal-promote-login" v-bind:title="$t('Label-Prompt-Login')">
         {{$t('Message-Prompt-Login')}} <br/>
         <template v-slot:modal-footer="{ ok, hide }">
           <a class="btn-sm btn btn-primary" href="/auth/mediawiki/login">{{$t('Label-Login')}}</a>
