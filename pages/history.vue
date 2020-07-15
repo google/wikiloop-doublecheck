7e93f2743ea4fb478d047046afe727fc4358a145
Zainan Victor Zhou <zzn@google.com>
2020-07-14 18:57:17 -0700

    feat(history): add new history page to replace /marked. fixes: #304.

--- pages/history.vue
+++ pages/history.vue
@@ -8,14 +8,22 @@
     <template v-else>
       <div class="card my-1" v-for="(item, i) in interactions">
       <div class="card-body">
-        <div class="d-flex">
-        <user-avatar-with-name :wikiUserName="item.wikiUserName" :userGaId="item.userGaId"></user-avatar-with-name>
-        <div style="line-height: 32px">{{$t('Label-ReviewedAndSays', item.wikiRevId)}}
-        <span :class="['badge', {
-          'badge-success': item.judgement === 'LooksGood',
-          'badge-secondary': item.judgement === 'NotSure',
-          'badge-danger': item.judgement === 'ShouldRevert'
-        }]">{{item.judgement}}</span>.</div>
+        <div class="d-flex justify-content-between">
+          <div class="d-flex justify-content--start">
+            <user-avatar-with-name :wikiUserName="item.wikiUserName" :userGaId="item.userGaId"></user-avatar-with-name>
+            <div style="line-height: 32px">
+              <span v-html="$t('Label-ReviewedAndSays', {
+                wikiRevId: item.wikiRevId,
+                href: `/revision/${item.wikiRevId.split(':')[0]}/${item.wikiRevId.split(':')[1]}`
+                })"></span>
+              <span :class="['badge', {
+                'badge-success': item.judgement === 'LooksGood',
+                'badge-secondary': item.judgement === 'NotSure',
+                'badge-danger': item.judgement === 'ShouldRevert'
+              }]">{{item.judgement}}</span>
+              <timeago :datetime="item.timestamp * 1000"></timeago>.
+            </div>
+          </div>
         </div>
       </div>
       </div>
@@ -42,11 +50,14 @@
 
   })
   export default class HistoryPage extends Vue {
+    loading:boolean = false;
     interactions: InteractionProps[];
     public parseWikiRevId = parseWikiRevId;
     mounted() {
       socket.on('interaction-props', async (interaction: InteractionProps) => {
+        this.loading = true;
         this.interactions = [interaction].concat(this.interactions); // push at the head.
+        this.loading = false;
       });
     }
   }
