fb85c3bad1716c4051019a8353d4b34c8a03b944
Zainan Victor Zhou <zzn@google.com>
2020-07-28 17:31:24 -0700

    feat(us2020): reduce crawl latency and migrate dev to atlas
    reduce crawl latency from 2h to 1min by 1. do not crawl diff because it takes 1 query per revision.
    2. bulk update the mongodb for each 50 revisions. 3. migrate to atlas mongodb since mlab is shut
    down.
    
    re: #305, #313

--- tscmd/dev/populate-revisions-given-title-cmd.ts
+++ tscmd/dev/populate-revisions-given-title-cmd.ts
@@ -3,11 +3,56 @@
 
 import { MwActionApiClient } from '@/shared/mwapi';
 import { logger } from '@/server/common';
-import { RevisionInfo } from '@/shared/models/revision-info.model';
-import { FeedRevision } from '~/shared/models/feed-revision.model';
+import { RevisionInfo, RevisionInfoProps } from '@/shared/models/revision-info.model';
+import { FeedRevision, FeedRevisionProps } from '~/shared/models/feed-revision.model';
 import { articleLists } from './data/us-2020';
 
 const Bottleneck = require("bottleneck");
+const getPair = function (wiki, result, pageId) {
+  let revisions = result.query.pages[pageId].revisions;
+  if (revisions.length > 1) {
+    logger.info(`revisions.length > 1
+title=${result.query.pages[pageId].title},
+revId=${revisions[0].revid}
+length=${revisions.length}`);
+  }
+  let revision = revisions[0];
+  let wikiRevId = `${wiki}:${revision.revid}`;
+  let revisionInfo = <RevisionInfoProps>{
+    wikiRevId: wikiRevId,
+    revId: revision.revid,
+    wiki: wiki,
+    pageId: parseInt(pageId),
+    title: result.query.pages[pageId].title,
+    wikiCreated: new Date(revision.timestamp),
+    tags: revision.tags,
+    summary: revision.comment,
+    // skip diff
+    // skip ores_damaging
+    // skip ores_damaging
+    prevRevId: revision.parentid
+  };
+  if (Object.keys(revision).indexOf('anon') /* anonymous */ >= 0) {
+    revisionInfo.anonymousIp = revision.user
+  } else revisionInfo.wikiUserName = revision.user
+  if (revision.oresscores.damaging) revisionInfo.ores_damaging = revision.oresscores.damaging.true;
+  if (revision.oresscores.badfaith) revisionInfo.ores_badfaith = revision.oresscores.goodfaith.false;
+
+  // let diffRet =  await neck.schedule(async () => await MwActionApiClient.getDiffByWikiRevId(wiki, revision.revid));
+  // if (diffRet.compare) {
+  //   revisionInfo.diffHtml = diffRet.compare["*"];
+  // } else {
+  //   logger.warn(`The diff doesn't exist for  ${revisionInfo.wikiRevId} diffRet = ${JSON.stringify(diffRet, null, 2)}`);
+  // }
+
+  let feedRevision = <FeedRevisionProps>{
+    feed: 'US2020',
+    wikiRevId: wikiRevId,
+    wiki: wiki,
+    feedRankScore: 0,
+  };
+  return [revisionInfo, feedRevision]
+};
 
 const populateRevisionsGivenTitleMain = async function () {
   let neck = new Bottleneck({
@@ -23,15 +68,14 @@ const populateRevisionsGivenTitleMain = async function () {
   console.log(`Connecting mongodb ...`);
   await mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
   console.log(`Connected mongodb!`);
-  mongoose.set('debug', true);
-  RevisionInfo.on('index', function(err) {
-      if (err) {
-          logger.error('RevisionInfo index error: %s', err);
-      } else {
-          logger.info('RevisionInfo indexing complete');
-      }
+  // mongoose.set('debug', true);
+  RevisionInfo.on('index', function (err) {
+    if (err) {
+      logger.error('RevisionInfo index error: %s', err);
+    } else {
+      logger.info('RevisionInfo indexing complete');
+    }
   });
-
 
   for (let articleIndex = 0; articleIndex < articleLists.length; articleIndex += 50) {
     let titles = articleLists.slice(articleIndex, Math.min(articleIndex + 50, articleLists.length));
@@ -39,62 +83,39 @@ const populateRevisionsGivenTitleMain = async function () {
 
     let wiki = 'enwiki';
     let result = await neck.schedule(async () => await MwActionApiClient.getLastRevisionsByTitles(titles, wiki));
-    let countC = 0;
+
     if (Object.keys(result.query.pages).length > 0) {
       let pageIds = Object.keys(result.query.pages);
+      let revisionInfos = [];
+      let feedRevisions = [];
       for (let pageId of pageIds) {
         if (result.query.pages[pageId].revisions?.length > 0) {
-          let revisions = result.query.pages[pageId].revisions;
-          if (revisions.length > 1) {
-            logger.info(`revisions.length > 1
-  title=${result.query.pages[pageId].title},
-  revId=${revisions[0].revid}
-  length=${revisions.length}`);
-          }
-          let revision = revisions[0];
-          let wikiRevId = `${wiki}:${revision.revid}`;
-          logger.info(`${countC} Fetching wikiRevId = ${wikiRevId}, title = ${result.query.pages[pageId].title}, diff = https://en.wikipedia.org/wiki/Special:Diff/${revision.revid}}.`);
-          if ((await RevisionInfo.count({wikiRevId: wikiRevId})) == 0) {
-            let revisionInfo = new RevisionInfo({
-              wikiRevId: wikiRevId,
-              revId: revision.revid,
-              wiki: wiki,
-              pageId: parseInt(pageId),
-              title: result.query.pages[pageId].title,
-              wikiCreated: new Date(revision.timestamp),
-              tags: revision.tags,
-              summary: revision.comment,
-              // skip diff
-              // skip ores_damaging
-              // skip ores_damaging
-              prevRevId: revision.parentid
-            });
-
-            if (Object.keys(revision).indexOf('anon') /* anonymous */ >= 0) {
-              revisionInfo.anonymousIp = revision.user
-            } else revisionInfo.wikiUserName = revision.user
-            if (revision.oresscores.damaging) revisionInfo.ores_damaging = revision.oresscores.damaging.true;
-            if (revision.oresscores.badfaith) revisionInfo.ores_badfaith = revision.oresscores.goodfaith.false;
-
-            let diffRet =  await neck.schedule(async () => await MwActionApiClient.getDiffByWikiRevId(wiki, revision.revid));
-            if (diffRet.compare) {
-              revisionInfo.diffHtml = diffRet.compare["*"];
-            } else {
-              logger.warn(`The diff doesn't exist for  ${revisionInfo.wikiRevId} diffRet = ${JSON.stringify(diffRet, null, 2)}`);
-            }
-            await revisionInfo.save();
-          }
-          if ((await FeedRevision.count({wikiRevId: wikiRevId})) == 0) {
-            let feedRevision = new FeedRevision({
-              feed: 'US2020',
-              wikiRevId: wikiRevId,
-              wiki: wiki,
-              feedRankScore: 0,
-            });
-            await feedRevision.save();
-          }
+          let [revisionInfo, feedRevision] = getPair(wiki, result, pageId);
+          revisionInfos.push(revisionInfo);
+          feedRevisions.push(feedRevision);
         }
       }
+      let ret = await Promise.all([
+        RevisionInfo.bulkWrite(revisionInfos.map((ri: RevisionInfoProps) => {
+          return {
+            updateOne: {
+              filter: { wikiRevId: ri.wikiRevId },
+              update: { $set: ri },
+              upsert: true
+            }
+          }
+        })),
+        FeedRevision.bulkWrite(feedRevisions.map((fr: FeedRevisionProps)=>{
+          return {
+            updateOne: {
+              filter: { wikiRevId: fr.wikiRevId },
+              update: { $set: fr },
+              upsert: true
+            }
+          }
+        }))
+      ]);
+      console.log(`Current articleIndex=${articleIndex} Ret = `, JSON.stringify(ret, null, 2));
     }
   }
   console.log(`done`);
