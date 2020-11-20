dc4e73e71b8989695becdbc6b5dc8e3687ff51af
Zainan Victor Zhou <zzn@google.com>
2020-11-20 05:43:32 -0800

    refactor(diffbox): rename PureDiffBox and minor update css.

--- components/PureDiffBox2.stories.js
+++ components/PureDiffBox2.stories.js
@@ -1,25 +1,27 @@
-import PureDiffBox from '@/components/PureDiffBox';
+import PureDiffBox2 from '~/components/PureDiffBox2';
 
 export default {
-  title: 'PureDiffBox',
-  component: PureDiffBox,
+  title: 'PureDiffBox2',
+  component: PureDiffBox2,
 }
 
 const Template = (args, { argTypes }) => ({
-  components: { PureDiffBox },
+  components: { PureDiffBox2 },
   props: Object.keys(argTypes),
-  template: '<PureDiffBox :diffSafeHtml="inputDiffHtml" />',
+  template: '<PureDiffBox2 :loading="loading" :diffSafeHtml="diffSafeHtml" />',
 });
 
 
 export const empty = Template.bind({});
 empty.args = {
-  inputDiffHtml: ``,
+  loading: false,
+  diffSafeHtml: ``,
 };
 
 export const basic = Template.bind({});
 basic.args = {
-  inputDiffHtml: `<tr>
+  loading: false,
+  diffSafeHtml: `<tr>
   <td colspan="2" class="diff-lineno">Line 32:</td>
   <td colspan="2" class="diff-lineno">Line 32:</td>
 </tr>
@@ -56,3 +58,20 @@ basic.args = {
 
 <!-- diff cache key enwiki:diff:wikidiff2:1.12:old-946272067:rev-976551791:1.10.0 -->`,
 };
+
+export const loading = Template.bind({});
+loading.args = {
+  loading: true,
+  diffSafeHtml: `<tr>
+  <td class="diff-marker">&#160;</td>
+  <td class="diff-context"><div>}}</div></td>
+  <td class="diff-marker">&#160;</td>
+  <td class="diff-context"><div>}}</div></td>
+</tr>`
+};
+
+export const beforeLoading = Template.bind({});
+beforeLoading.args = {
+  loading: true
+};
+
---
+++ components/PureDiffBox2.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="diff-box">
+    <template v-if="loading">
+        <loading-indicator></loading-indicator>
+    </template>
+    <template v-else-if="!diffSafeHtml">
+      <div class="diff-box__nodiff">
+        <!-- TODO change to includ i18n, add styling. -->
+        <i class="fas fa-check-circle mr-1"></i>There is no diff.
+      </div>
+
+    </template>
+    <template v-else>
+
+      <div class="diff-box__header">
+        <!-- <h5>{{ $t('Label-OriginalWikitext') }}</h5>
+        <h5>{{ $t('Label-ChangedWikitext') }}</h5> -->
+      </div>
+      <table class="diff-box__table">
+        <tbody v-html="diffSafeHtml"></tbody>
+      </table>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue} from 'nuxt-property-decorator';
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+
+@Component({
+  components: {
+      LoadingIndicator
+  }
+})
+export default class PureDiffBox2 extends Vue {
+  @Prop({ type: String }) readonly diffSafeHtml: string;
+  @Prop({ type: Boolean}) readonly loading: boolean;
+}
+</script>
+<style lang="scss" scoped>
+.diff-box {
+  min-height: 10rem;
+  min-width: 20rem;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+
+  &__nodiff {
+    animation: fadeIn .5s 3s;
+    font-size: 2rem;
+    color: var(--secondary);
+    i {
+      color: var(--success);
+
+    }
+  }
+
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+</style>
